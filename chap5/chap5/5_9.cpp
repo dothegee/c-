65be73a1a79ad6b935a897fd4233b507d0e16cc2
dothegee <sjuinobat@gmail.com>
2023-05-08 11:08:30 +0900

    복사 생성자

---
+++ chap5/chap5/5_9.cpp
@@ -0,0 +1,32 @@
+//복사 생성자
+
+#include <iostream>
+using namespace std;
+
+class Circle {
+private:
+	int radius;
+public:
+	Circle(const Circle& c);
+	Circle() {
+		radius = 1;
+	}
+	Circle(int radius) {
+		this->radius = radius;
+	}
+	double getArea() {
+		return 3.14 * radius * radius;
+	}
+};
+Circle::Circle(const Circle& c) {
+	this->radius = c.radius;
+	cout << "복사 생성자 실행 radius = " << radius << endl;
+}
+
+int main() {
+	Circle src(30);
+	Circle dest(src);
+
+	cout << "원본 " << src.getArea() << endl;
+	cout << "사본 " << dest.getArea() << endl;
+}
