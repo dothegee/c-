512d442470e66b22ca7ceaec4946cac452fe7aed
dothegee <sjuinobat@gmail.com>
2023-05-10 14:32:19 +0900

    전위 ++ 연산자 작성

---
+++ ch7_프렌즈와연산자/ch7_프렌즈와연산자/7_8.cpp
@@ -0,0 +1,35 @@
+//단항 연산자 중복
+// 전위 연산자 
+//전위 ++ 연산자 중복
+#include <iostream>
+using namespace std;
+
+class Power {
+	int kick;
+	int punch;
+public:
+	Power(int kick = 0, int punch = 0) {
+		this->kick = kick;
+		this->punch = punch;
+	}
+	void show();
+	Power& operator++ ();
+};
+
+void Power::show() {
+	cout << "kick = " << kick << ", " << "punch = " << punch << endl;
+}
+Power& Power::operator++() {
+	kick++;
+	punch++;
+	return *this;
+}
+
+int main() {
+	Power a(3, 5), b;
+	a.show();
+	b.show();
+	b = ++a;
+	a.show();
+	b.show();
+}
